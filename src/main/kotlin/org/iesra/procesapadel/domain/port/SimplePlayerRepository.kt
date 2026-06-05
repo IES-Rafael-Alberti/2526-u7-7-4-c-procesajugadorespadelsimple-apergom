67399d8bdc72785b5a7f7166823a6bba48ca5f1c
apergom <aperezg@iesrafaelalberti.es>
2026-06-05 10:24:03 +0200

    rehecho repository

--- src/main/kotlin/org/iesra/procesapadel/domain/port/SimplePlayerRepository.kt
+++ src/main/kotlin/org/iesra/procesapadel/domain/port/SimplePlayerRepository.kt
@@ -7,7 +7,14 @@ import kotlin.io.path.listDirectoryEntries
 class SimplePlayerRepository {
 
     fun findInputFiles(path: Path): List<File> {
-       return path.listDirectoryEntries("*.txt") as List<File>
+        val carpeta = path.toFile().listFiles()
+        val archivos = mutableListOf<File>()
+        for (file in carpeta) {
+            if (file.isFile) {
+                archivos.add(file)
+            }
+        }
+        return archivos
     }
 
     fun moveToProcessed(file: File) {
