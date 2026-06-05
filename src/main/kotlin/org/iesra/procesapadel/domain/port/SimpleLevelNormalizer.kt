40cc7fb7940802b0df79f0725c1b83eedec7490c
apergom <aperezg@iesrafaelalberti.es>
2026-06-05 10:39:41 +0200

    realizacion de la funcion de normalizacion de niveles

--- src/main/kotlin/org/iesra/procesapadel/domain/port/SimpleLevelNormalizer.kt
+++ src/main/kotlin/org/iesra/procesapadel/domain/port/SimpleLevelNormalizer.kt
@@ -1,9 +1,16 @@
 package org.iesra.procesapadel.domain.port
 
 import org.iesra.procesapadel.domain.model.Player
+import java.util.Locale.getDefault
 
 class SimpleLevelNormalizer {
     fun normalize(player:Player) {
+        if ((player.nivel != "iniciación") || (player.nivel != "intermedio") || (player.nivel != "experto")) {
 
+        } else {
+            if (player.nivel == "iniciación"){
+                player.nivel = player.nivel.uppercase(getDefault())
+            }
+        }
     }
 }
